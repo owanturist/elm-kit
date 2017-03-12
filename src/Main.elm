48f39ed7343082a80a5f4c4f24c285f025aefad2
Anton Ovechkin <owanturist@gmail.com>
2017-03-12 10:00:53 +0700

    Making program very simple

--- src/Main.elm
+++ src/Main.elm
@@ -1,61 +1,21 @@
-module Main exposing (..)
+module Main exposing (main)
 
 import Html
     exposing
         ( Html
         , program
         , div
-        , ul
-        , li
-        , button
-        , input
         , text
         )
-import Html.Attributes
-    exposing
-        ( type_
-        , name
-        , checked
-        )
-import Html.Events exposing (onClick, onCheck)
 
 
 type alias Model =
-    List Cube
-
-
-type alias Cube =
-    { role : Role
-    , sides : List Side
-    , active : Maybe Side
-    }
-
-
-type Role
-    = Result
-    | EventResult
-    | Event
-
-
-type Side
-    = One
-    | Two
-    | Three
-    | Four
-    | Five
-    | Six
-    | Green
-    | Blue
-    | Yellow
-    | Pirates
+    Int
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( [ Cube Result [ One, Two, Three, Four, Five, Six ] Nothing
-      , Cube EventResult [ One, Two, Three, Four, Five, Six ] Nothing
-      , Cube Event [ Green, Blue, Yellow, Pirates ] Nothing
-      ]
+    ( 0
     , Cmd.none
     )
 
@@ -77,164 +37,17 @@ subscriptions _ =
 
 view : Model -> Html Msg
 view model =
-    div [] (List.map viewCube model)
-
-
-viewCube : Cube -> Html Msg
-viewCube cube =
-    ul [] (List.map (viewSide cube) cube.sides)
-
-
-getRadioName : Role -> String
-getRadioName role =
-    case role of
-        Result ->
-            "result"
-
-        EventResult ->
-            "event-result"
-
-        Event ->
-            "event"
-
-
-viewSide : Cube -> Side -> Html Msg
-viewSide cube side =
-    let
-        active =
-            case cube.active of
-                Just activeSide ->
-                    activeSide == side
-
-                Nothing ->
-                    False
-    in
-        li [] <|
-            case side of
-                One ->
-                    [ text "1"
-                    , input
-                        [ type_ "radio"
-                        , name <| getRadioName cube.role
-                        , checked active
-                        , onCheck (Select cube.role side)
-                        ]
-                        []
-                    ]
-
-                Two ->
-                    [ text "2"
-                    , input
-                        [ type_ "radio"
-                        , name <| getRadioName cube.role
-                        , checked active
-                        , onCheck (Select cube.role side)
-                        ]
-                        []
-                    ]
-
-                Three ->
-                    [ text "3"
-                    , input
-                        [ type_ "radio"
-                        , name <| getRadioName cube.role
-                        , checked active
-                        , onCheck (Select cube.role side)
-                        ]
-                        []
-                    ]
-
-                Four ->
-                    [ text "4"
-                    , input
-                        [ type_ "radio"
-                        , name <| getRadioName cube.role
-                        , checked active
-                        , onCheck (Select cube.role side)
-                        ]
-                        []
-                    ]
-
-                Five ->
-                    [ text "5"
-                    , input
-                        [ type_ "radio"
-                        , name <| getRadioName cube.role
-                        , checked active
-                        , onCheck (Select cube.role side)
-                        ]
-                        []
-                    ]
-
-                Six ->
-                    [ text "6"
-                    , input
-                        [ type_ "radio"
-                        , name <| getRadioName cube.role
-                        , checked active
-                        , onCheck (Select cube.role side)
-                        ]
-                        []
-                    ]
-
-                Green ->
-                    [ text "green"
-                    , input
-                        [ type_ "radio"
-                        , name <| getRadioName cube.role
-                        , checked active
-                        , onCheck (Select cube.role side)
-                        ]
-                        []
-                    ]
-
-                Blue ->
-                    [ text "blue"
-                    , input
-                        [ type_ "radio"
-                        , name <| getRadioName cube.role
-                        , checked active
-                        , onCheck (Select cube.role side)
-                        ]
-                        []
-                    ]
-
-                Yellow ->
-                    [ text "yellow"
-                    , input
-                        [ type_ "radio"
-                        , name <| getRadioName cube.role
-                        , checked active
-                        , onCheck (Select cube.role side)
-                        ]
-                        []
-                    ]
-
-                Pirates ->
-                    [ text "pirates"
-                    , input
-                        [ type_ "radio"
-                        , name <| getRadioName cube.role
-                        , checked active
-                        , onCheck (Select cube.role side)
-                        ]
-                        []
-                    ]
+    div []
+        [ text (toString model)
+        ]
 
 
 type Msg
-    = Select Role Side Bool
+    = NoOp
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Select role side checked ->
-            let
-                selectCube cube =
-                    if cube.role == role then
-                        { cube | active = Just side }
-                    else
-                        cube
-            in
-                ( List.map selectCube model, Cmd.none )
+        NoOp ->
+            ( model, Cmd.none )
